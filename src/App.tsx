6b408da91f796d7c47086fda1a1b63ca68a4df3f
Acrata <hxtr227@gmail.com>
2022-06-12 19:50:44 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import '@fontsource/epilogue/400.css'
+import Intro from './components/Intro'
+
+function App (): JSX.Element {
+  return (
+    <div className='App'>
+      <Intro />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Intro.tsx
@@ -0,0 +1,7 @@
+const Intro = (): JSX.Element => {
+  return (
+    <h1>Intro section</h1>
+  )
+}
+
+export default Intro
